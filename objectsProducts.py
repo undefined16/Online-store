619ce14b82a5f8633ec82bb5be15ce046985dc1f
undefined16 <fjdjdjfnnfmf@gmail.com>
2024-10-17 22:21:18 +0300

    objectsProducts

---
+++ objectsProducts.py
@@ -0,0 +1,12 @@
+from productsManager import Product, ProductManager
+
+productManager = ProductManager()
+
+product1 = Product("Яблука", 2.99, 50, 101)
+product2 = Product("Банани", 1.99, 30, 102)
+product3 = Product("Апельсини", 3.49, 20, 103)
+productManager.addProduct(product1)
+productManager.addProduct(product2)
+productManager.addProduct(product3)
+
+productManager.viewProducts()
