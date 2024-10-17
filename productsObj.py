1d1b5bf6ce9df2d409af599bce90d4635bf0863e
EvGeN180 <malyna.evgen2010@gmail.com>
2024-10-17 23:56:20 +0300

    obj

---
+++ productsObj.py
@@ -0,0 +1,8 @@
+from productsManager import Product,ProductManager
+product1 = Product("Яблука", 2.99, 50, 101)
+product2 = Product("Банани", 1.99, 30, 102)
+product3 = Product("Апельсини", 3.49, 20, 103)
+# manager.addProduct(product1)
+# manager.addProduct(product2)
+# manager.addProduct(product3)
+product1.addProduct()
